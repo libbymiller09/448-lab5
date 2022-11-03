846bf22f1bf5622e842902ee75a19a28cd0684c7
Elisabeth Miller <34798877+libbymiller09@users.noreply.github.com>
2022-11-03 11:44:08 -0500

    Update index.js

--- 448-lab5/cssManipulation/index.js
+++ 448-lab5/cssManipulation/index.js
@@ -1,8 +1,14 @@
 function changecolors() {
   var paragraph = document.getElementsByTagName('p');
-  var borderInput = document.getElementById('1');
-  var backgroundColorInput = document.getElementById('2');
+  var redborder = document.getElementById('redInpBorder').value;
+  var greenborder = document.getElementById('greenInpBorder').value;
+  var blueborder = document.getElementById('blueInpBorder').value;
+  var widthborder = document.getElementById('widthInpBorder').value;
+  var redbg = document.getElementById('redInpBg').value;
+  var greenbg = document.getElementById('greenInpBg').value;
+  var bluebg = document.getElementById('blueInpBg').value;
 
-  paragraph.border = "";
-  paragraph.backgroundColor = "";
+  paragraph.style.borderColor = 'rgb('+redborder+', '+greenborder+', '+blueborder+')';
+  paragraph.style.borderWidth = widthborder;
+  paragraph.style.backgroundColor = 'rgb('+redbg+', '+greenbg+', '+bluebg+')';
 }
