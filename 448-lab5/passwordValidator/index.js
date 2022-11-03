363fdee28cea25667aa3cd03ccaf42a4889a9a91
Elisabeth Miller <34798877+libbymiller09@users.noreply.github.com>
2022-11-03 11:45:11 -0500

    Update index.js

--- 448-lab5/passwordValidator/index.js
+++ 448-lab5/passwordValidator/index.js
@@ -1,10 +1,12 @@
 function validate() {
-  var inputOne = document.getElementById('passwordOne');
-  var inputTwo = document.getElementById('passwordTwo');
-  if (inputOne != inputTwo) {
-    alert("Passwords don't match!");
-  }
-  if (inputOne.length() < 8) {
+  var inputOne = document.getElementById('passwordOne').value;
+  var inputTwo = document.getElementById('passwordTwo').value;
+ 
+  if (inputOne.length < 8) {
     alert("Password needs to be at least 8 characters!")
+    if (inputOne != inputTwo) {
+      alert("Passwords don't match!");
+    }
   }
+  alert("Password Valid!");
 }
